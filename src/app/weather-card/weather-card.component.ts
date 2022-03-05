c4bb7f21e56d542cd24f7e9f41c94e2373b93145
khushi-dev-1822 <saini29khushi@gmail.com>
2022-03-05 15:52:29 +0530

    api initiated

--- src/app/weather-card/weather-card.component.ts
+++ src/app/weather-card/weather-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios, { Axios } from 'axios';
 
 @Component({
   selector: 'app-weather-card',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather-card.component.css']
 })
 export class WeatherCardComponent implements OnInit {
+  public cityName!: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    axios({
+      method: 'get',
+      url: 'http://api.weatherapi.com/v1/forecast.json?key=9466d8d9fff649658ff113923212407&q=india&days=10&aqi=no&alerts=yes',
+      responseType: 'stream'
+    })
+      .then(function (response) {
+        const cityName = response.data.location.name;
+       console.log(cityName);
+      });
+  }
+  public getApiData() {
+    
   }
 
 }
